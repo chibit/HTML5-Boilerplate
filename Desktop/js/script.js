50e869fa9654747eb0ae224d20cdb4042bce0b83
Matthew Sanderson <matthew@sandersons.id.au>
2012-02-25 19:35:27 +1000

    Updates to way Tables are accommodated
    My solution to tables in an adaptive/responsive site. jQuery takes care
    of wrapping any table in the appropriate class. Tables resize but do
    not shrink content or wrap headings, then become scrollable/swipable.

--- Desktop/js/script.js
+++ Desktop/js/script.js
@@ -1,10 +1,18 @@
-/* Author:
-
+/*
+modules
 */
 
+	/*responsive tables*/
+	/*tables are automatically contained within a wrapping class, .tablecontainer. This ensures that the tables do no resize but are made scrollable via overflow:scroll. Default styling for tables is located within elements.less*/
+	$(function()
+		{
+			var table=$('table');
+			$(table)
+				.wrap('<div class="tablecontainer">')
+					$('.tablecontainer')
+						.after('<!-- @.tablecontainer-->');
+		}
+	);
+	/*@-@*/
 
-
-
-
-
-
+/*@--@*/
