ed7129a52a2c990d581d59c7453ed9efb7de2942
Matthew Sanderson <matthew@sandersons.id.au>
2012-03-06 20:00:51 +1000

    Updates to script.js
    Made the responsive tables scripting recursive, more efficient.

--- Desktop/js/script.js
+++ Desktop/js/script.js
@@ -6,23 +6,52 @@ modules
 	/*tables are automatically contained within a wrapping class, .tablecontainer. This ensures that the tables do no resize but are made scrollable via overflow:scroll. Default styling for tables is located within elements.less*/
 	$(function()
 		{
-			var table=$('table');
-			var tableinstructionswidth=
+			var table=$('table'),
+			tablerows=$('table > tbody > tr'),
+			tablecontainer=$('.tablecontainer'),
+			tableinstructions=$('.tableinstructions'),
+			inner=$('.inner');
+			
 			$(table)
-				.wrap('<div class="tablecontainer">')
-					$('.tablecontainer')
-						.append('<div class="tableinstructions">Swipe to browse</div><!-- @.tableinstructions-->')
+				.wrap('<div class="inner">')
+				
+				$(table).parent('inner')
+					.wrap('<div class="tablecontainer">')
+					.after('<!-- @.inner-->')
+					.end();
+					
+					$('tablecontainer')
+						.append('<div class="tableinstructions"></div><!-- @.tableinstructions-->')
 						.after('<!-- @.tablecontainer-->')
 						.end();
-						if(table.width() > ($('.tablecontainer').width()))
-							{
-								$('.tableinstructions')
-									.css('display','block')
-								$(table)
-									.css('margin-left',$('.tableinstructions').width());
-							}
+						
+						$('tablecontainer')
+							.each(function(i)
+								{
+									if($(this).find(table).width() > ($(this).width()))
+										{
+											$(this)
+												.find('tableinstructions')
+													.css('display','block')
+											$(this)
+												.find('inner')
+													.css('margin-right',$('tableinstructions').width());
+										}
+								}
+							);
 		}
 	);
 	/*@-@*/
+	
+	/*remove rows*/
+	/*a commonly used prototyping tool for simulating removing rows from a table*/
+	$('.removerow')
+		.click(function()
+			{
+				$(this)
+					.parents('tr')
+					.remove();
+			}
+		);
 
 /*@--@*/
--- Mobile/js/script.js
+++ Mobile/js/script.js
@@ -6,13 +6,52 @@ modules
 	/*tables are automatically contained within a wrapping class, .tablecontainer. This ensures that the tables do no resize but are made scrollable via overflow:scroll. Default styling for tables is located within elements.less*/
 	$(function()
 		{
-			var table=$('table');
+			var table=$('table'),
+			tablerows=$('table > tbody > tr'),
+			tablecontainer=$('.tablecontainer'),
+			tableinstructions=$('.tableinstructions'),
+			inner=$('.inner');
+			
 			$(table)
-				.wrap('<div class="tablecontainer">')
-					$('.tablecontainer')
-						.after('<!-- @.tablecontainer-->');
+				.wrap('<div class="inner">')
+				
+				$(table).parent('inner')
+					.wrap('<div class="tablecontainer">')
+					.after('<!-- @.inner-->')
+					.end();
+					
+					$('tablecontainer')
+						.append('<div class="tableinstructions"></div><!-- @.tableinstructions-->')
+						.after('<!-- @.tablecontainer-->')
+						.end();
+						
+						$('tablecontainer')
+							.each(function(i)
+								{
+									if($(this).find(table).width() > ($(this).width()))
+										{
+											$(this)
+												.find('tableinstructions')
+													.css('display','block')
+											$(this)
+												.find('inner')
+													.css('margin-right',$('tableinstructions').width());
+										}
+								}
+							);
 		}
 	);
 	/*@-@*/
+	
+	/*remove rows*/
+	/*a commonly used prototyping tool for simulating removing rows from a table*/
+	$('.removerow')
+		.click(function()
+			{
+				$(this)
+					.parents('tr')
+					.remove();
+			}
+		);
 
 /*@--@*/
